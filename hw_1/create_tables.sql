8d8793cd291b820097276bb9948dc4d4b4989699
axreldable <starikovaleksei1992@mail.ru>
2020-06-14 17:27:27 +0300

    hw_1: mv languages into separate table

--- hw_1/create_tables.sql
+++ hw_1/create_tables.sql
@@ -1,5 +1,8 @@
 drop table if exists hw_1;
 drop table if exists titles;
+drop table if exists titles_tmp;
+drop table if exists languages;
+drop table if exists languages_tmp;
 
 create temp table if not exists titles_tmp
 (
@@ -20,11 +23,34 @@ insert into titles
 select distinct on (title) *
 from titles_tmp;
 
+create temp table if not exists languages_tmp
+(
+    id       serial primary key not null,
+    language varchar(30)
+);
+
+create table if not exists languages
+(
+    id       serial primary key not null,
+    language varchar(30)
+);
+
+COPY languages_tmp (language)
+    FROM PROGRAM 'cut -d "," -f 4 /tmp/input_data/some_customers.csv' WITH (FORMAT CSV, HEADER);
+
+delete from languages_tmp
+where language is null;
+
+insert into languages
+select distinct on (language) *
+from languages_tmp;
+
 create table if not exists hw_1
 (
     id                      serial primary key not null,
     title                   varchar(30)        not null,
     title_id                integer,
+    language_id             integer,
     first_name              varchar(50)        not null,
     last_name               varchar(50)        not null,
     correspondence_language varchar(50),
@@ -37,7 +63,8 @@ create table if not exists hw_1
     city                    varchar(50)        not null,
     street                  varchar(100)       not null,
     building_number         varchar(50)        not null,
-    FOREIGN KEY (title_id) REFERENCES titles (id)
+    FOREIGN KEY (title_id) REFERENCES titles (id),
+    FOREIGN KEY (language_id) REFERENCES languages (id)
 );
 
 COPY hw_1 (title, first_name, last_name, correspondence_language, birth_date, gender, marital_status, country,
@@ -49,5 +76,11 @@ set title_id = titles.id
 from titles
 where hw_1.title = titles.title;
 
+update hw_1
+set language_id = languages.id
+from languages
+where hw_1.correspondence_language = languages.language;
+
 alter table hw_1
-    drop column title;
+    drop column title,
+    drop column correspondence_language;
